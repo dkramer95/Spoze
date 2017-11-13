e8315a88c213da93f62224acb06e41be96735f39
David Kramer <dkramer@student.neumont.edu>
2017-11-12 17:41:12 -0700

    Enables some textures w/ alpha to overlap 	* Doesn't work for semi-transparent however

--- app/src/main/res/raw/texture_fragment_shader.glsl
+++ app/src/main/res/raw/texture_fragment_shader.glsl
@@ -5,5 +5,11 @@ varying vec2 v_TextureCoordinates;
 
 void main() {
     // u_TextureUnit contains actual texture data
-    gl_FragColor = texture2D(u_Texture, v_TextureCoordinates);
+    vec4 val = texture2D(u_Texture, v_TextureCoordinates);
+    if (val.a > 0.5) {
+        gl_FragColor = val;
+    } else {
+        discard;
+    }
+//    gl_FragColor = texture2D(u_Texture, v_TextureCoordinates);
 }
