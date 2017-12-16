cac3e50fcedef74112c01338daa58ffb76188d93
David Kramer <dkramer@student.neumont.edu>
2017-12-15 17:47:05 -0700

    System nav icons are dimmer in VisualizationActivity

--- app/src/main/java/edu/neumont/dkramer/spoze3/VisualizationActivity.java
+++ app/src/main/java/edu/neumont/dkramer/spoze3/VisualizationActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
@@ -105,6 +106,7 @@ public class VisualizationActivity extends GLCameraActivity implements Screensho
 	protected void onResume() {
 		super.onResume();
 		checkSharedImage();
+		dimNavIcons();
 	}
 
 	protected void init() {
@@ -229,10 +231,15 @@ public class VisualizationActivity extends GLCameraActivity implements Screensho
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+	}
+
+	protected void dimNavIcons() {
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
 	}
 
 	@Override
